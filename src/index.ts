4dbcfbf082e0f63cabf0e51d5e28ca981a6b2361
Lamis Abdullah <102637669+lamisab@users.noreply.github.com>
2022-06-27 06:26:06 +0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { server } from './server';
+
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3000;
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	});
